53e236b2594a79cc11614fb11f69f368fe13f05d
MarioBlancosoto <ytalykual@gmail.com>
2022-08-07 12:04:11 +0200

    inicio proyecto

---
+++ src/main/java/com/jlmstore/JlmstoreApplication.java
@@ -0,0 +1,13 @@
+package com.jlmstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JlmstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JlmstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlmstore/JlmstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlmstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JlmstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
